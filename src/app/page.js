98caff5cfec7deee871196efec6e201cd3906bfb
sazib07 <sazibmd93@gmail.com>
2025-11-12 04:18:47 +0600

    ui added

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,238 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Home page</div>
+  <>
+ <div className="bg-gray-100 px-4 py-8">
+  <div className="mx-auto lg:max-w-6xl md:max-w-4xl">
+    <div className="border-b border-gray-300 pb-4">
+      <h2 className="text-slate-900 text-2xl font-bold">Trending Products</h2>
+      <p className="text-slate-600 mt-2">
+        Out the most popular and trending products.
+      </p>
+    </div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 min-[1200px]:!grid-cols-6 gap-4 mt-8">
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/watch1.webp"
+              alt="product1"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              French Timex
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$95.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/product14.webp"
+              alt="product2"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Echo Elegance
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$20.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/laptop4.webp"
+              alt="product3"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Acer One 14 AMD
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$400.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/watch4.webp"
+              alt="product4"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+          French Timex
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$11.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/coffee7.webp"
+              alt="product5"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+                        Irish Cream Dream
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$90.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/watch7.webp"
+              alt="product6"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Smart Watch
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$110.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/watch8.webp"
+              alt="product7"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Creative Watch
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$50.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/laptop2.webp"
+              alt="product8"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              ASUS Vivobook 15
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$450.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/watch3.webp"
+              alt="product1"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              French Timex
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$95.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className=" w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/product14.webp"
+              alt="product2"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Echo Elegance
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$20.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-[140/84] aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/laptop4.webp"
+              alt="product3"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Acer One 14 AMD
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$400.00</h4>
+          </div>
+        </a>
+      </div>
+      <div className="bg-white p-3 cursor-pointer shadow-sm border border-gray-200 rounded-md hover:scale-[1.03] transition-all">
+        <a href="javascript:void(0)" className="block">
+          <div className="sm:aspect-140/84 aspect-[140/100] w-full overflow-hidden mx-auto">
+            <img
+              src="https://readymadeui.com/images/watch5.webp"
+              alt="product4"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <hr className="my-4 border-gray-300" />
+          <div className="text-center">
+            <h3 className="text-sm font-semibold text-slate-900">
+              Smart Watch
+            </h3>
+            <h4 className="text-sm text-blue-600 font-bold mt-2">$11.00</h4>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+      
+    </>
   )
 }
 
