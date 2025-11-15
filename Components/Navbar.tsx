e75f53a97bfd49a71a5b70ed9258509a94b0e42b
sazib07 <sazibmd93@gmail.com>
2025-11-16 03:05:00 +0600

    navbar common fixed

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const navbar = () => {
   return (
- <>
-  {/* Include this script tag or install `@tailwindplus/elements` via npm: */}
-  {/*  */}
-  <nav className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+ 
+   //@ts-ignore
+  <nav className="relative bg-slate-800 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div className="relative flex h-16 items-center justify-between">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
           {/* Mobile menu button*/}
+          
           <button
+         
             type="button"
+    
             command="--toggle"
             commandfor="mobile-menu"
             className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500"
@@ -92,6 +94,7 @@ const navbar = () => {
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <button
             type="button"
+            //@ts-ignore
             className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
           >
             <span className="absolute -inset-1.5" />
@@ -113,41 +116,7 @@ const navbar = () => {
             </svg>
           </button>
           {/* Profile dropdown */}
-          <el-dropdown className="relative ml-3">
-            <button className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">Open user menu</span>
-              <img
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-                className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
-              />
-            </button>
-            <el-menu
-              anchor="bottom end"
-              popover=""
-              className="w-48 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-            >
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden"
-              >
-                Your profile
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden"
-              >
-                Settings
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden"
-              >
-                Sign out
-              </a>
-            </el-menu>
-          </el-dropdown>
+         
         </div>
       </div>
     </div>
@@ -182,7 +151,7 @@ const navbar = () => {
       </div>
     </el-disclosure>
   </nav>
-</>
+
 
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
-import Navbar from './../../Components/Navbar';
+
 
 
 const page = () => {
   return (
   <>
- <Navbar/>
+
 <Link href="/blog">Blog</Link>
       
     </>
