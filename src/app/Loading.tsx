f4835a085dc251aa8b077bcd0e043a01243954eb
sazib07 <sazibmd93@gmail.com>
2025-11-28 18:50:15 +0600

    Loading designed

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Loading = () => {
+const loading = () => {
   return (
     <div className="w-full h-screen bg-teal-300 text-white text-center">Loading</div>
   )
 }
 
-export default Loading
+export default loading
