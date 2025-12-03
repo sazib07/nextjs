f3af7dcb68d648e3c17aa69f4851c97636a21c1b
sazib07 <sazibmd93@gmail.com>
2025-12-03 23:40:59 +0600

    blog page update

--- src/app/(root)/blog/page.tsx
+++ src/app/(root)/blog/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="bg-gray-900 py-24 sm:py-32">
+    <div className="bg-gray-800 py-24 sm:py-32">
   <div className="mx-auto max-w-7xl px-6 lg:px-8">
     <div className="mx-auto max-w-2xl lg:mx-0">
       <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">
